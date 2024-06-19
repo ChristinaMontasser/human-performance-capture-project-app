f81fc80de539d1e72d6ea3b77f62c63247c98f4c
raniyas <raniyashams1@gmail.com>
2024-06-19 13:43:58 +0200

    v2.1

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from routes.run_model import run_model_blueprint
+from routes.list_containers import list_containers_blueprint
+from routes.start_model import start_container_blueprint
 from config import Config
 
 #Creat flask appliction
@@ -8,6 +10,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 #We usually divide the flask application into modules, each modules is a defined as a blueprint 
 #Then we register blueprints that are related to the flask app 
+app.register_blueprint(list_containers_blueprint)
+app.register_blueprint(start_container_blueprint)
 app.register_blueprint(run_model_blueprint)
 
 if __name__ == '__main__':
